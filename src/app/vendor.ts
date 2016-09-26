134e013cd87a67a6ff7a8f7224689fad9a9990dd
Tobias Pickel <tobias.pickel@rsu-reifen.de>
2016-09-26 20:56:32 +0200

    init app

---
+++ src/app/vendor.ts
@@ -0,0 +1,21 @@
+// Angular
+import '@angular/platform-browser';
+import '@angular/core';
+import '@angular/common';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/router';
+
+// RxJS
+// Uncomment below if you use RxJS in many feature modules
+// import 'rxjs';
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// import 'bootstrap/dist/css/bootstrap.css';
+
+import 'font-awesome/css/font-awesome.css';
+
+// import 'jquery';
+// import 'bootstrap/dist/js/bootstrap';
+
+import '../public/styles/main.scss';
